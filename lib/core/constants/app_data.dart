d90d0592c60fc54e55ab534070f4eeb701c69481
subhajit735 <subhajit735@gmail.com>
2023-07-08 19:12:16 +0530

    url_constants added

--- lib/core/constants/app_data.dart
+++ lib/core/constants/app_data.dart
@@ -1,6 +1,9 @@
 class AppData {
   static String appName = "Grace Soft";
 
+  static String accessToken =
+      "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyIjoiSmVzc3UiLCJEZXNpZ25hdGlvbiI6IiIsIkdyb3VwSUQiOiI4IiwiUHJvcGVydHlJRCI6IjE4ODUiLCJVc2VyTmFtZSI6InByYWthc2giLCJpYXQiOjE2ODg4MjA4OTAsImV4cCI6MTY4ODg2NDA5MCwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdCIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3QifQ.qPsIbEBGHdjjH4nNrrlhCfKMJMVDXAyhWkY-2JNypR0";
+
   static List<String> daysItems = ['Today', 'Tomorrow'];
 
   static List<String> searchByItems = [
---
+++ lib/core/constants/url_constant.dart
@@ -0,0 +1,13 @@
+//=========================Base url ================================
+
+const String BASE_URL = "https://gracebeta.com";
+
+//=======================Dashboard======================================
+
+const String DASHBOARD_COUNTS =
+    "$BASE_URL/PMSUI-test3/mig/webapi/api/Lists/GetCounts";
+
+//=========================Calender Api================================
+
+const String INITIAL_DATA =
+    "$BASE_URL/PMSUI-test3/mig/webapi/api/Calendar/GetCalendarData";
